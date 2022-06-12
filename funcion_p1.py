a386c368f60766675cb56d414dfbe614a294d26a
primercado <primercado90@gmail.com>
2022-06-12 12:55:13 -0300

    funcion p1

---
+++ funcion_p1.py
@@ -0,0 +1,13 @@
+# Función P1
+# Retorna el producto de los 2 primero más el 3er parámetros, usando las funciones anteriores
+
+val1=int(input("Por favor, ingrese el primer número que desea sumar: "))
+val2=int(input("Por favor, ingrese el segundo número que desea sumar: "))
+val3=int(input("Por favor, ingrese el tercer número que desea sumar: "))
+
+
+
+def suma (x, y):
+    return x + y
+
+print("La suma de sus números da el valor de: ",suma(val1, val2) + val3)
